6147a5f2219bd2ebaa80a31d1003ba0f74002afe
GagarinAP <gagarin.web@gmail.com>
2016-07-11 18:51:14 +0300

    edit

--- app.js
+++ app.js
@@ -1,60 +1,36 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var about = require('./routes/about');
+var path = require('path');
+var keysModule = require('./module/module.js');
+var appView = require('./module/appView.js');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/about', about);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/workers', function (req, res) {
+    res.send(keysModule.getAllWorkers());
 });
 
-// error handlers
+app.get('/search', function (req, res) {
+  res.send(appView.searchRecord(req.query));
+});
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+app.get('/chart', function (req, res) {
+  res.send(appView.getChartData());
+});
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.post('/record', function (req, res) {
+    res.send(appView.addRecord(req.body)) ;
 });
 
 
-module.exports = app;
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var app = express();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+app.get('/workers', function (req, res) {
+    res.send(keysModule.getAllWorkers());
+});
+
+app.get('/search', function (req, res) {
+  res.send(appView.searchRecord(req.query));
+});
+
+app.get('/chart', function (req, res) {
+  res.send(appView.getChartData());
+});
+
+app.post('/record', function (req, res) {
+    res.send(appView.addRecord(req.body)) ;
 });
 
 module.exports = router;
